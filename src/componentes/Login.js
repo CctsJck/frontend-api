39b124366f2fba962b2f2b0e8e7babb0a6e83e8f
gnb2000 <barirodriguez@gmail.com>
2021-10-15 13:49:43 -0300

    Mejora del login

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -12,24 +12,28 @@ function Login(){
                     <div className="col-4"> 
                         <div class="card bg-dark text-white h-100">
                             <img src="https://cdn.discordapp.com/attachments/759833803433705516/898579175861018694/diputados-maradonajpg.png" class="card-img-top" alt="..."/>
-                            <div class="card-img-overlay">
-                                <h5 class="card-title text-center">JUGADORES</h5>
+                            <div class="card-img-overlay vision">
+                                <h2 class="card-title text-center ">JUGADORES</h2>
+                                <a href="#" class="stretched-link"></a>
                             </div>
+
                         </div>
                     </div>
                     <div className="col-4"> 
                     <div class="card bg-dark text-white h-100">
                             <img src="https://blogs.rockyview.ab.ca/wp-content/uploads/2018/04/admin-prof-day.jpg" class="card-img-top" alt="..."/>
-                            <div class="card-img-overlay">
-                                <h5 class="card-title text-center">Administrador</h5>
+                            <div class="card-img-overlay vision">
+                                <h2 class="card-title text-center">ADMIN</h2>
+                                <a href="#" class="stretched-link"></a>
                             </div>
                         </div>
                     </div>
                     <div className="col-4"> 
                     <div class="card bg-dark text-white h-100">
                             <img src="https://cdn.discordapp.com/attachments/759833803433705516/898596337380130886/RepresentanteFutbol.png" class="card-img-top" alt="..."/>
-                            <div class="card-img-overlay">
-                                <h5 class="card-title text-center">Representante</h5>
+                            <div class="card-img-overlay vision">
+                                <h2 class="card-title text-center">REPRESENTANTE</h2>
+                                <a href="#" class="stretched-link"></a>
                             </div>
                         </div>
                     </div>
