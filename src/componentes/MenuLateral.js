8848080afe54034d4bfffe89588563b1753aabd5
gnb2000 <barirodriguez@gmail.com>
2021-10-19 08:09:32 -0300

    Mejora del diseño

--- src/componentes/MenuLateral.js
+++ src/componentes/MenuLateral.js
@@ -6,7 +6,7 @@ function MenuLateral(){
     const parametros = useParams();
 
     return(
-        <div className="text-center main bg-dark text-white">
+        <div className="colorFondo">
             {/*<div class="nav flex-column ">
                 { parametros.id == 1 ? 
                     <div>
@@ -45,7 +45,7 @@ function MenuLateral(){
                     </nav>
                 </div> : <div></div>}
                 
-                </div>*/}
+                </div>
 
                 <ul class="nav justify-content-center p-4">
                 <li class="nav-item">
@@ -58,7 +58,26 @@ function MenuLateral(){
                     <a class="nav-link text-white" href="#">Representante   </a>
                 </li>
                 
-                </ul>
+                </ul>*/}
+  <nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white" href="#">FUADE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link text-white" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">LOGOUT</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
 
 
         </div>
