752c786e04ca7765a6425c84ac66c9cd9196080e
gnb2000 <barirodriguez@gmail.com>
2021-10-27 15:55:55 -0300

    Nuevas funcionalidades

--- src/componentes/admin/AdminCampeonatos.js
+++ src/componentes/admin/AdminCampeonatos.js
@@ -1,7 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 
 function AdminCampeonatos(){
+
+    const [campeonatos,setCampeonatos] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/obtenerCampeonatos")
+            .then(response => {
+                setCampeonatos(response.data);
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    },[])
+
+
+    function activarCampeonato(id){
+        console.log(id);
+    }
+
+    function desactivarCampeonato(id){
+        console.log(id);
+    }
     
 
     return (
@@ -11,7 +34,7 @@ function AdminCampeonatos(){
                 <table class="table mt-5">
                     <thead>
                         <tr>
-                        <th scope="col">#</th>
+                        <th scope="col">ID</th>
                         <th scope="col">Nombre</th>
                         <th scope="col">Fecha inicio</th>
                         <th scope="col">Fecha fin</th>
@@ -25,7 +48,33 @@ function AdminCampeonatos(){
                         </tr>
                     </thead>
                     <tbody>
-                        
+                        {campeonatos != [] ? campeonatos.map(campeonato => {
+                            return(
+                                <tr key={campeonato.idCampeonato}>
+                                    <th>{campeonato.idCampeonato}</th>
+                                    <td>{campeonato.descripcion}</td>
+                                    <td>{campeonato.fechaInicio}</td>
+                                    <td>{campeonato.fechaFin}</td>
+                                    <td>{campeonato.tipo}</td>
+                                    <td>{campeonato.categoria}</td>
+                                    <td>{campeonato.estado == "activo" ? <span class="badge bg-success">Activo</span> : <span class="badge bg-danger">Inactivo</span>}</td>
+                                    <td>{campeonato.estado == "inactivo" ? 
+
+                                    <button onClick={() => activarCampeonato(campeonato.idCampeonato)} className="btn btn-success me-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                            <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                                        </svg>
+                                    </button> : 
+                                    
+                                    <button onClick={() => desactivarCampeonato(campeonato.idCampeonato)} className="btn btn-danger disabled">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/>
+                                            <path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/>
+                                        </svg>
+                                    </button>}</td>
+                                </tr>
+                            )
+                        }) : null}
                     </tbody>
                 </table>
 
