1bd52d560c408f506d69cac6221b631d3119d265
gnb2000 <barirodriguez@gmail.com>
2021-11-01 16:30:15 -0300

    cargarResultadoPartido terminado

--- src/componentes/admin/CargarResultadoPartido.js
+++ src/componentes/admin/CargarResultadoPartido.js
@@ -9,14 +9,21 @@ function CargarResultadoPartido(){
     /* Variables */
     const[partido,setPartido] = useState('-1');
     const[partidos,setPartidos] = useState([]);
-    const [goles,setGoles] = useState([]);
     const[campeonatos,setCampeonatos] = useState([]);
     const[campeonato,setCampeonato] = useState('-1');
     const[jugador,setJugador] = useState('-1');
-    const[jugadores,setJugadores] = useState([]);
+    const[listaJugadoresPartido,setlistaJugadoresPartido] = useState([]);
 
     const [partidoConEquipos,setPartidosConEquipos] = useState([]);
-    
+    const [partidoConJugadores,setPartidoConJugadores] = useState([]);
+    const [jugadores, setJugadores] = useState([]);
+
+    const [tipoSuceso, setTipoSuceso] = useState("falta");
+    const [tipoGol,setTipoGol] = useState("");
+    const [tipoFalta,setTipoFalta] = useState("");
+    const [minuto, setMinuto] = useState(-1);
+
+
 
     useEffect(() => {
         axios.get('http://localhost:8080/obtenerCampeonatos')
@@ -36,7 +43,6 @@ function CargarResultadoPartido(){
     },[campeonato])
 
     useEffect(() => { 
-        console.log(partidos);
         partidos.map( async part => {
             //AXIOS HTTP GET ANIDADOS
             axios.get("http://localhost:8080/getClubPorId?idClub="+part.local)
@@ -57,15 +63,31 @@ function CargarResultadoPartido(){
     },[partidos])
 
     useEffect(() => {
-        jugadores.map( async jug => {
-    
-            axios.get("http://localhost:8080/obtenerJugadoresPartido?idPartido="+partido)
+        partidos.map( par => {
+            
+            axios.get("http://localhost:8080/obtenerJugadoresPartido?idPartido="+par.idPartido)
             .then(response => {
-                setJugadores(response.data);
+                setlistaJugadoresPartido(response.data);
+                
+                
+                
             })
-        }
-            )
-    })
+        })
+    },[partido])
+
+    useEffect(() => {
+        listaJugadoresPartido.map(jug => {
+            axios.get("http://localhost:8080/getJugadorPorId?idJugador="+jug.idJugador)
+            .then(response => {
+                setJugadores(jugadores => ([...jugadores,response.data]));
+            })
+        })
+        
+    },[listaJugadoresPartido])
+
+    useEffect(() => {
+        console.log(jugadores);
+    },[jugadores])
     
 
     
@@ -86,13 +108,39 @@ function CargarResultadoPartido(){
     }
 
     function handleMinutoChange(e){
+        setMinuto(e.target.value);
+        console.log(e.target.value);
 
     }
 
-    function handleTipoChange(e){
-
+    function handleSucesoChange(e){
+        setTipoSuceso(e.target.value);
     }
+
+    function handleTipoFaltaChange(e){
+        setTipoFalta(e.target.value);
+        
+    }
+
+    function handleTipoGolChange(e){
+        setTipoGol(e.target.value)
+    }
+
     function handleSubmit(e){
+        e.preventDefault();
+        console.log(e.target.value);
+        if (tipoSuceso === "falta"){
+            axios.post("http://localhost:8080/agregarFaltaJugador?idJugador="+jugador+"&idPartido="+partido+"&idCampeonato="+campeonato+"&minuto="+minuto+"&tipo="+tipoFalta)
+            .then(response => {
+                return toast.success("Falta agregado con exito");
+            })
+        } else {
+            axios.post("http://localhost:8080/agregarGolJugador?idJugador="+jugador+"&idPartido="+partido+"&minuto="+minuto+"&tipo="+tipoGol)
+            .then(response => {
+                return toast.success("Gol agregado con exito");
+            })
+
+        }
 
     }
 
@@ -101,6 +149,7 @@ function CargarResultadoPartido(){
 
 return(
     <div className = "container">
+        <ToastContainer/>
         <h2>Cargar resultado de un partido</h2>
         <div className="col-6 mt-5">
             <form onSubmit={handleSubmit}>
@@ -118,36 +167,37 @@ return(
                 <div class="mb-3">
                         <label for="par label" class="form-label">Seleccione el Partido</label>
                         <select class="form-select" id="partidos" onChange={handlePartidoChange}aria-label= "partidos" >
+                            <option value="-1">Seleccione el partido</option>
+
                             {partidoConEquipos.map(part => {
                                 
                                 return(
-                                    <option value={part.partido.id}>{part.clubL.nombre} VS {part.clubV.nombre}</option>
+                                    <option value={part.partido.idPartido}>{part.clubL.nombre} VS {part.clubV.nombre}</option>
                                 )
                             }
                             )}
                         </select>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
-                        <label class="form-check-label" for="flexRadioDefault1">
-                            Agregar Gol
-                        </label>
+                    <input class="form-check-input" type="radio" name="agregarFalta" value="falta" onChange={handleSucesoChange} id="agregarFalta" checked={tipoSuceso === "falta"}/>
+                    <label class="form-check-label" for="agregarFalta">
+                        Agregar falta
+                    </label>
                 </div>
-                <div class="mb-3">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
-                        <label class="form-check-label" for="flexRadioDefault2">
-                              Agregar Falta
-                        </label>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="agregarGol" value="gol" onChange={handleSucesoChange} id="agregarGol" checked={tipoSuceso === "gol"}/>
+                    <label class="form-check-label" for="agregarGol">
+                        Agregar gol
+                    </label>
                 </div>
 
-            </form>
             <div class="mb-3">
                 <label for="nombreJugador" class="form-label">Nombre del Jugador</label>
                 <select class="form-select" id="jugadores" onChange={handleJugadorChange}aria-label= "partidos" >
                     <option value="-1">Jugadores</option>
                     {jugadores.map(jug => {
                         return(
-                            <option value={jug.idJugador}> </option>
+                            <option value={jug.idJugador}> {jug.nombre} {jug.apellido}</option>
                         )
                     }
                         )}
@@ -160,19 +210,31 @@ return(
                 <input type="text" class="form-control" onChange={handleMinutoChange} id="minuto" placeholder="Tiempo de Juego" aria-describedby="minuto"/>
             </div>
 
+            {tipoSuceso === "gol" ? 
             <div class="mb-3">
                 <label for="tipo" class="form-label">Tipo de Gol</label>
-                <select class="form-select"  aria-label= "tipoGoles" >
+                <select class="form-select" onChange={handleTipoGolChange} aria-label= "tipoGoles" >
                 <option value="-1">Seleccione el tipo</option>
-                    <option>A favor</option>
-                    <option>Encontra</option>
+                    <option value="a favor">A favor</option>
+                    <option value="en contra">En contra</option>
                 </select>
 
-            </div>
+            </div> : <div class="mb-3">
+                <label for="tipo" class="form-label">Tipo de falta</label>
+                <select class="form-select" onChange={handleTipoFaltaChange} aria-label= "tipoFalta" >
+                    <option value="-1">Seleccione el tipo</option>
+                    <option value="amarilla">Amarilla</option>
+                    <option value="roja">Roja</option>
+                </select>
+
+            </div>}
+
+            
 
             <div className="mb-5">
                 <button type="submit" className="btn btn-success">Agregar</button>
             </div>
+        </form>
 
 
 
